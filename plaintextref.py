13228a359c957ae7a54f7a53c1f0bd8308fb61ec
K Kollmann <git@k.kollmann.moe>
2015-02-08 15:31:29 +0100

    check if file type provided by user is supported

--- plaintextref.py
+++ plaintextref.py
@@ -24,3 +24,25 @@
 # and output the dictionary sorted by keys (ascending)
 # like so: [key] dictionarycontent
 #
+
+import fileinput
+import sys
+import os
+
+# check file type of file input by user via cli
+# + convert extension to lower case just in case
+filename, extension = os.path.splitext(sys.argv[-1])
+extension = extension.lower()
+
+if extension == ".txt":
+    # code for text files goes here
+    print("text!")
+elif extension == (".htm" or ".html"):
+    # code for HTML files goes here
+    print("html!")
+elif extension == ".md":
+    # code for Markdown files goes here
+    print("markdown!")
+else:
+    print("You did not specify a valid file name.\n"
+        "Only .txt, .htm/.html and .md files can be converted.")
