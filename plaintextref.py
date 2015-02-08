a09467e7c62a0b009feab9d76ce52eb8658c6e77
K Kollmann <git@k.kollmann.moe>
2015-02-08 14:56:52 +0100

    add TODOs for basic functionality of program

---
+++ plaintextref.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+# TODO
+# 
+# check file type of file input via cli
+# differentiate between:
+#   plain text (.txt)
+#   HTML (.htm, .html)
+#   Markdown (.md)
+#
+# loop through all lines of the file
+# search for occurrences of brackets in each line
+# use round brackets only if they contain URLs of any kind (://)
+# use all square brackets
+#
+# remove any whitespace preceding the brackets
+# replace each pair of brackets with [i]
+# where i is a counter that gets incremented
+#
+# save contents of brackets in a dictionary (?) 
+# with counter variable serving as key
+#
+# add two new lines at end of file
+# and output the dictionary sorted by keys (ascending)
+# like so: [key] dictionarycontent
+#
